24939623f0f1116e326e632382100aeabb704716
Nomi <totalbrokecoding@gmail.com>
2026-01-25 06:35:46 +0500

    'setting-barrel-file-export'

---
+++ src/app/components/navigation/navigation.js
@@ -0,0 +1,3 @@
+export default function NavBar(){
+    return <button className="bg-primary text-primary-foreground w-2xs h-10 hover:cursor-pointer">I will be a button</button>
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
+import { NavBar } from "./components";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <>
+  <h1>Hello World</h1>
+  <NavBar/>
+  </>;
 }
