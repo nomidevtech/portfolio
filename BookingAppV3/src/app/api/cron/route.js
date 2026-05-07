2f5c606d1df5dfe830fe1fdf7c08a7b28113458e
nomidevtech <totalbrokecoding@gmail.com>
2026-05-07 05:02:58 +0500

    cron-job

---
+++ BookingAppV3/src/app/api/cron/route.js
@@ -0,0 +1,32 @@
+
+
+export function GET(request) {
+    const authHeader = request.headers.get('authorization');
+    const cronSecret = process.env.CRON_SECRET;
+    console.log("API route was called after env before check");
+
+    console.log("authHeader:", authHeader);
+    console.log("cronSecret:", cronSecret);
+
+    if (!cronSecret || authHeader !== `Bearer ${cronSecret}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
+
+    console.log("API route was called after env");
+
+    return Response.json({ success: true, message: "Hello from API" });
+}
+
+
+
+
+
+// export default async function GET() {
+//     console.log("API route was called");
+
+//     return Response.json({
+//         message: "Hello from API"
+//     });
+// }
